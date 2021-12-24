b66cf08898c1de8e0b493eafde15065c8f095d61
Fajar Alfiantino <91827168+fajaralf@users.noreply.github.com>
2021-12-24 14:35:37 +0700

    Add files via upload

---
+++ AppLibrary/app_library/src/test/java/com/ibik/library/app/dashboard/DashboardTest.java
@@ -0,0 +1,14 @@
+package com.ibik.library.app.dashboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DashboardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
